5b78f9ab5616273f02cc9abe2f5cc0a976204d79
mezianechauvin <44549022+mezianechauvin@users.noreply.github.com>
2024-05-09 18:07:29 -0400

    Remove folder

--- mmm_ed_clean/update_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-> .env
-echo "CURRENT_USERNAME=eddie.deane@greenchef.com" >> .env
-echo "VAULT_TOKEN=$(vault login -address=https://vault.secrets.hellofresh.io -method=oidc -namespace=infrastructure/data-science-spew -token-only)" >> .env
